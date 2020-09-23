8be6fa4998f530178b9d34a5b8f43996088adfdf
= <=>
2020-09-23 23:05:35 +0500

    change in design

--- src/data/static-data.js
+++ src/data/static-data.js
@@ -9,20 +9,20 @@ export const frontmatter = {
     {
       title: "TVShop",
       desc: "Tweet with ",
-      hostLink: "https://https://gatsbytvshop.netlify.app/",
-      repoLink: "https://https://github.com/syedkamal3262/TVShop",
+      hostLink: "https://gatsbytvshop.netlify.app/",
+      repoLink: "https://github.com/syedkamal3262/TVShop",
     },
     {
       title: "WhatsApp-Clone",
       desc: "Minimal JavaScript type checking library",
-      hostLink: "https://https://whatsapp-clone-b34ae.firebaseapp.com/",
-      repoLink: "https://https://github.com/syedkamal3262/WhatsApp-Clone",
+      hostLink: "https://whatsapp-clone-b34ae.firebaseapp.com/",
+      repoLink: "https://github.com/syedkamal3262/WhatsApp-Clone",
     },
     {
       title: "Netflix-Clone",
       desc: "React library ",
-      hostLink: "https://https://netflix-clone-2ba22.firebaseapp.com/",
-      repoLink: "https://https://github.com/syedkamal3262/Netflix-Clone",
+      hostLink: "https://netflix-clone-2ba22.firebaseapp.com/",
+      repoLink: "https://github.com/syedkamal3262/Netflix-Clone",
     },
   ],
 }
